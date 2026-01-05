ea6da6e7796746cb9fa776cd59b488cb840088ce
ranjith67 <ranjithmadival678@gmail.com>
2026-01-05 12:08:38 +0530

    feat: Implement navigation and add Playground and History screens with AsyncStorage integration

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import PlaygroundScreen from './screens/PlaygroundScreen';
+import HistoryScreen from './screens/HistoryScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export type RootStackParamList = {
+    Playground: { code?: string };
+    History: undefined;
+};
+
+const Tab = createBottomTabNavigator<RootStackParamList>();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Playground" component={PlaygroundScreen}
+        options={{
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons name="code-slash" size={size} color={color} />
+              ),
+        }}
+        />
+        <Tab.Screen name="History" component={HistoryScreen} 
+        options={{
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons name="time" size={size} color={color} />
+              ),
+        }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
